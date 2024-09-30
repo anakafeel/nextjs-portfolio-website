7da9ca2ac186712159fc4a23318df4d301150454
saim hashmi <hashmisaim037@gmail.com>
2024-09-30 12:24:27 -0400

    Blog page Added

--- src/slices/index.ts
+++ src/slices/index.ts
@@ -4,6 +4,8 @@ import dynamic from "next/dynamic";
 
 export const components = {
   biography: dynamic(() => import("./Biography")),
+  content_index: dynamic(() => import("./ContentIndex")),
   homepage: dynamic(() => import("./Homepage")),
   tech_list: dynamic(() => import("./TechList")),
+  text_blog: dynamic(() => import("./TextBlog")),
 };
