6d52e28e2be1c026f40e8a2b25a27436aa271eec
anakafeel <hashmisaim037@gmail.com>
2025-10-21 00:03:27 +0000

    fix: remove apollo script
    fix: remove apollo script
    
    fix: remove apollo script
    
    fix: remove apollo script
    
    fix: remove apollo script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { SpeedInsights } from '@vercel/speed-insights/next';
-import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "My Portfolio Website",
@@ -24,26 +23,6 @@ export default function RootLayout({
         <div className="pointer-events-none absolute inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-20 mix-blend-soft-light"></div>
         <Footer />
         <SpeedInsights />
-        {/* Apollo tracker script */}
-        <Script
-          id="apollo-tracker"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-function initApollo(){
-  var n = Math.random().toString(36).substring(7),
-      o = document.createElement("script");
-  o.src = "https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache=" + n;
-  o.async = !0; o.defer = !0;
-  o.onload = function(){
-    window.trackingFunctions.onLoad({appId: "68f2d532f4913e0015e27e1e"});
-  };
-  document.head.appendChild(o)
-}
-initApollo();
-            `
-          }}
-        />
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,3 +22,4 @@ export async function generateMetadata(): Promise<Metadata> {
 }
 
 /* new about image added */
+/* test commit  */
